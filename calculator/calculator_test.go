e75fc07154745a6eaec41924a4b4da51250ecea7
forfd8960 <forfd8960@gmail.com>
2021-08-06 22:48:49 +0800

    calculator test

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -10,6 +10,10 @@ func TestCalculator(t *testing.T) {
 	t.Run("simple math", test1Plus2)
 	t.Run("left and right parent", leftAndRightParent)
 	t.Run("one parentheses", oneParentheses)
+	t.Run("left parentheses not match", parenthesesNotMatch)
+	t.Run("right parentheses not match", rightParenthesesNotMatch)
+	t.Run("unsupport operator", unsupportOperator)
+	t.Run("divisor is zero", divisorIsZero)
 }
 
 func test1Plus2(t *testing.T) {
@@ -43,4 +47,28 @@ func oneParentheses(t *testing.T) {
 	if assert.Nil(t, err) {
 		assert.Equal(t, float64(90), result)
 	}
+}
+
+func parenthesesNotMatch(t *testing.T) {
+	cal := NewCalculator("(1 + 2))")
+	err := cal.Exec()
+	assert.Equal(t, errInvalidExpression, err)
+}
+
+func rightParenthesesNotMatch(t *testing.T) {
+	cal := NewCalculator("((1 + 2)")
+	err := cal.Exec()
+	assert.Equal(t, errInvalidExpression, err)
+}
+
+func unsupportOperator(t *testing.T) {
+	cal := NewCalculator("(1 + 2) % (10 * 9)")
+	err := cal.Exec()
+	assert.Equal(t, errUnsupportToken('%'), err)
+}
+
+func divisorIsZero(t *testing.T) {
+	cal := NewCalculator("10 / 0")
+	err := cal.Exec()
+	assert.Equal(t, errDivisorZero, err)
 }
